8334d168b769e1eea9709376131404a4af6d5459
Sanakey Augustine <sanakeyaugustineayiku@gmail.com>
2020-03-04 16:27:29 +0000

    created migrations

--- database/migrations/2020_03_04_161536_create_meat_sales_table.php
+++ database/migrations/2020_03_04_161536_create_meat_sales_table.php
@@ -15,7 +15,13 @@ class CreateMeatSalesTable extends Migration
     {
         Schema::create('meat_sales', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('farm_id');
+            $table->timestamp('date');
+            $table->decimal('quantity');
+            $table->decimal('price');
             $table->timestamps();
+            $table->primary(['id','farm_id']);
+            $table->foreign('farm_id')->references('id')->on('farms')->onDelete('cascade');
         });
     }
 
